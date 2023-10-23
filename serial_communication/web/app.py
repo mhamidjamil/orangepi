dbc39c143c80dd4627bd3a06a9ea77d219501d87
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2023-10-24 00:28:48 +0500

    Staging (#19)
    * dynamic_port_selection_work
    
    * undefined_string_issue
    
    * baud_rate_selection_part_added
    
    * serial_terminal_scroll_feature
    
    * serial_data_input_show_with_different_color

--- serial_communication/web/app.py
+++ serial_communication/web/app.py
@@ -1,27 +1,60 @@
 from flask import Flask, render_template, request
 import serial
+import serial.tools.list_ports
 
 app = Flask(__name__)
-# Replace with your actual serial port
-ser = serial.Serial('/dev/ttyACM0', 115200)
 
+# Function to get a list of available serial ports
+# Modify the get_serial_ports function
+def get_serial_ports():
+    return [{'port': port.device, 'baud_rate': 115200} for port in serial.tools.list_ports.comports() if port.device.startswith('/dev/tty')]
+
+# Replace with the default serial port
+default_serial_port = '/dev/ttyACM0'
+ser = None
+
+# Attempt to open the default serial port
+try:
+    ser = serial.Serial(default_serial_port, 115200)
+except serial.SerialException:
+    pass  # Handle the exception if the default port is not accessible
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Get a list of available serial ports with default baud rate
+    available_ports = get_serial_ports()
+
+    # Baud rates to be displayed in the dropdown
+    baud_rates = [9600, 115200, 230400, 460800, 921600]
+
+    return render_template('index.html', default_port=default_serial_port, available_ports=available_ports, baud_rates=baud_rates)
 
 
 @app.route('/read_serial')
 def read_serial():
-    data = ser.readline().decode('utf-8')
-    return {'data': data}
+    if ser:
+        try:
+            data = ser.readline().decode('utf-8')
+            return {'data': data.strip()}  # Strip whitespace from the data
+        except UnicodeDecodeError:
+            return {'error': 'Error decoding serial data'}
+    else:
+        return {'error': 'Serial port not accessible'}
 
 
 @app.route('/send_serial', methods=['POST'])
 def send_serial():
-    data_to_send = request.form['data']
-    ser.write(data_to_send.encode('utf-8'))
-    return {'status': 'success'}
+    if ser:
+        # Get the selected baud rate from the form or use the default (115200)
+        baud_rate = int(request.form.get('baudRate', 115200))
+
+        # Update the serial connection baud rate
+        ser.baudrate = baud_rate
+        data_to_send = request.form['data']
+        ser.write(data_to_send.encode('utf-8'))
+        return {'status': 'success'}
+    else:
+        return {'error': 'Serial port not accessible'}
 
 
 if __name__ == '__main__':

8039330f43a95dc0a4b00f0f90dc37b0f7f492b0
M Hamid Jamil <m.hamid.jamil99@gmail.com>
2024-01-28 00:48:36 +0500

    First Deployment test

--- serial_communication/web/app.py
+++ serial_communication/web/app.py
@@ -1,11 +1,6 @@
-
-
-#$ last work 27/Jan/24 [03:00 AM]
-## version 2.0.6
-## Release Note : Added pylint action
-
 import time
 import sys
+import multiprocessing
 import threading
 from flask import Flask, render_template, request
 import serial
@@ -17,26 +12,21 @@ from routes.serial_handler import (
     is_ngrok_link_sent, send_message, fetch_current_time_online,
     exception_logger
 )
-
 from routes.routes import send_auth
+
 BG_TASK = True
 BOOT_MESSAGE_SEND = False
 
 app = Flask(__name__)
 
-# Function to get a list of available serial ports
-# Modify the get_serial_ports function
-
-
 def get_serial_ports():
+    """Return a list of available serial ports."""
     return [{'port': port.device, 'baud_rate': 115200}
             for port in serial.tools.list_ports.comports()
-                if port.device.startswith('/dev/tty')]
-
-# Function to update the serial port dynamically
-
+            if port.device.startswith('/dev/tty')]
 
 def update_serial_port():
+    """Update the serial port dynamically."""
     try:
         global BG_TASK, ser
         max_port_number = 5  # Maximum port number to try
@@ -55,112 +45,98 @@ def update_serial_port():
                 except serial.SerialException:
                     print(f"Port {port} not available. Trying the next one.")
 
-            print(
-                f"No available ports (tried up to {max_port_number}). Retrying in 10 seconds...")
+            print(f"No available ports (tried up to {max_port_number}). Retrying in 10 seconds...")
             time.sleep(10)
-            update_serial_port()  # TODO: this effect execution flow
-    except Exception as e:
-        exception_logger("update_serial_port", e)
-
+            update_serial_port()
+    except Exception as ex:
+        exception_logger("update_serial_port", ex)
 
 # Replace with the default serial port
 ser = update_serial_port()
 
-
 @app.route('/')
 def index():
-    # Get a list of available serial ports with default baud rate
+    """Render the index page."""
     available_ports = get_serial_ports()
-
-    # Baud rates to be displayed in the dropdown
     baud_rates = [9600, 115200, 230400, 460800, 921600]
-
     return render_template('index.html', default_port=ser, available_ports=available_ports, baud_rates=baud_rates)
-
 
 @app.route('/read_serial')
 def read_serial():
+    """Read serial data."""
     global BG_TASK
     try:
         if ser:
-            try:
-                data = ser.readline().decode('utf-8')
-                read_serial_data(data)
-
-                return {'data': data.strip()}  # Strip whitespace from the data
-            except UnicodeDecodeError:
-                return {'error': 'Error decoding serial data'}
-        else:
-            return {'error': 'Serial port not accessible'}
-    except serial.SerialException as e:
+            data = ser.readline().decode('utf-8')
+            read_serial_data(data)
+            return {'data': data.strip()}  # Strip whitespace from the data
+        return {'error': 'Serial port not accessible'}
+    except serial.SerialException as ex:
         BG_TASK = False
         update_serial_port()
-        exception_logger("read_serial", e)
+        exception_logger("read_serial", ex)
         return {'error': 'Error reading from serial port'}
-
 
 @app.route('/send_serial', methods=['POST'])
 def send_serial():
+    """Send data to the serial port."""
     try:
         data_to_send = request.form['data']
         ser.write(data_to_send.encode('utf-8'))
         return {'status': 'success'}
-    except Exception as e:
-        exception_logger("send_serial", e)
-
+    except Exception as ex:
+        exception_logger("send_serial", ex)
 
 @app.route('/send_auth', methods=['GET'])
 def send_auth_route():
+    """Send authentication."""
     return send_auth()
-
 
 if BG_TASK:
     schedule.every(2).minutes.do(update_time)
-    schedule.every(5).minutes.do(update_namaz_time)  # TODO: implement array
-
+    schedule.every(5).minutes.do(update_namaz_time)
 
 def update_schedule():
+    """Update the schedule."""
     while True:
-        schedule.run_pending()  # run background task after specified delay
+        schedule.run_pending()
         time.sleep(10)
 
-
 def one_time_task():
+    """Execute one-time tasks."""
     try:
         global BOOT_MESSAGE_SEND
-        # Delay the execution of send_ngrok_link() by 5 minutes
-
-        # Check if send_ngrok_link() has not been called yet
         if not is_ngrok_link_sent():
             time.sleep(10)
             if not BOOT_MESSAGE_SEND:
-                send_message("Orange Pi just boot-up time stamp: " +
-                             fetch_current_time_online())
-                BOOT_MESSAGE_SEND = True # pylint: disable=W0603
+                send_message("Orange Pi just boot-up time stamp: " + fetch_current_time_online())
+                BOOT_MESSAGE_SEND = True
             time.sleep(5)
             say_to_serial("sms sending?")
             time.sleep(5)
             send_ngrok_link()
-    except Exception as e:
-        exception_logger("one_time_task", e)
-
+    except Exception as ex:
+        exception_logger("one_time_task", ex)
 
 if __name__ == '__main__':
     try:
+
         if len(sys.argv) > 1:
-            # If there are no command-line arguments, assume it's called as a service
             print("\n-----------> Running as a service <-----------\n")
-            time.sleep(600)  # wait for some time until routers power on
+            time.sleep(600)
         else:
-            # If there are command-line arguments, assume it's called from the terminal
             time.sleep(3)
             print("\n-----------> Running from terminal <-----------\n")
+            
+        current_process_id = multiprocessing.current_process().pid
+        print(f"!!----------> Process ID: {current_process_id} - Executing my_function")
+
+        # app.run(host='0.0.0.0', port=6677)
+        app.run()
         thread = threading.Thread(target=update_schedule)
         thread2 = threading.Thread(target=one_time_task)
         thread.start()
         thread2.start()
 
-        # Run the Flask app
-        app.run(host='0.0.0.0', port=6677, debug=True)
-    except Exception as e:
-        print(f"Exception in main: {e}")
+    except Exception as ex:
+        print(f"Exception in main: {ex}")
--- serial_communication/web/routes/serial_handler.py
+++ serial_communication/web/routes/serial_handler.py
@@ -1,41 +1,41 @@
 # serial_handler.py
-
-import requests
+"""Module for handling serial communication."""
 import datetime
 import re
 import time
-from bs4 import BeautifulSoup
-from pyngrok import ngrok
 import subprocess
 import os
+from bs4 import BeautifulSoup
+from pyngrok import ngrok
 from dotenv import load_dotenv
 import serial
-ngrok_link_sent = False
+import requests
 
+load_dotenv()
+CURRENT_NGROK_LINK = ""
+LOGS_RECEIVING = False
+LOG_DATA = ""
+SERIAL_PORT = None
+NGROK_LINK_SENT = False
 EXCEPTION_LOGGER_FILE = "exception_logs"
 EXTENSION_TYPE = ".txt"
 DEFAULT_PORT = 8069
 
 
+
 def is_ngrok_link_sent():
-    global ngrok_link_sent
-    return ngrok_link_sent
+    """method to know if the NGROK link is send or not"""
+    return NGROK_LINK_SENT
 
 
 def set_serial_object(serial_object):
-    global ser
-    ser = serial_object
-
-
-load_dotenv()
-ngrok_link = ""
-logs_receiving = False
-log_data = ""
-
-ser = None
+    """Define serial port for this file"""
+    global SERIAL_PORT # pylint: disable=global-statement
+    SERIAL_PORT = serial_object
 
 
 def reboot_system():
+    """Used to reboot the system on message"""
     password = os.getenv("MY_PASSWORD")
     if not password:
         raise ValueError("Password not set in the .env file")
@@ -51,8 +51,9 @@ def reboot_system():
 
 
 def send_ngrok_link(target_port=None):
+    """Will send NGROK link via message"""
     try:
-        global ngrok_link
+        global CURRENT_NGROK_LINK, NGROK_LINK_SENT # pylint: disable=global-statement
         stop_ngrok()
         time.sleep(2)
         ngrok.set_auth_token(os.getenv("NGROK_TOKEN"))
@@ -70,32 +71,32 @@ def send_ngrok_link(target_port=None):
         if public_url:
             print(f"Ngrok URL is available: {public_url}")
             send_to_serial_port("sms " + public_url)
-            ngrok_link = public_url
-            global ngrok_link_sent
-            ngrok_link_sent = True
+            CURRENT_NGROK_LINK = public_url
+            NGROK_LINK_SENT = True
             # Perform other tasks with ngrok_url
         else:
             print("Failed to obtain Ngrok URL.")
             send_to_serial_port("sms Failed to obtain Ngrok URL.")
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-except
         print("NGROK not initialized")
         write_in_file("ngrok_logger", "\nERROR:\n" + str(e))
 
 
 def update_namaz_time():
+    """Will update update namaz time on TTGO-TCall"""
     try:
-        global current_time
+        current_time = ""
         # Replace this URL with the actual URL of the prayer times for Lahore
         url = os.getenv("LAHORE_NAMAZ_TIME")
 
         # Fetch the HTML content of the webpage
-        response = requests.get(url)
+        response = requests.get(url, timeout=10)
         soup = BeautifulSoup(response.content, 'html.parser')
 
         # Fetch the current time online
         try:
             response = requests.get(
-                'http://worldtimeapi.org/api/timezone/Asia/Karachi')
+                'http://worldtimeapi.org/api/timezone/Asia/Karachi', timeout=10)
             data = response.json()
             current_time = datetime.datetime.fromisoformat(data['datetime'])
             current_time = current_time.strftime("%I:%M %p")
@@ -107,20 +108,20 @@ def update_namaz_time():
         if not current_time:
             print("Failed to fetch current time online.")
             return
-        # else:
-            # print(f"Current time: {current_time}")
 
         # Find the prayer time row that corresponds to the next prayer after the current time
         prayer_times = soup.find_all('td', {'data-label': True})
         next_prayer = None
 
-        for time in prayer_times:
-            prayer_time = time.text.strip()
-            if datetime.datetime.strptime(prayer_time, '%I:%M %p') > datetime.datetime.strptime(current_time, '%I:%M %p'):
-                next_prayer = time['data-label']
+        for prayer_time in prayer_times:
+            prayer_time = prayer_time.text.strip()
+            if (datetime.datetime.strptime(prayer_time, '%I:%M %p') >
+                    datetime.datetime.strptime(current_time, '%I:%M %p')):
+
+                next_prayer = prayer_time['data-label']
                 break
 
-        # Print and send the next prayer time to the ser terminal
+        # Print and send the next prayer time to the SERIAL_PORT terminal
         if next_prayer:
             message = f"{next_prayer}: {prayer_time}"
             print(message)
@@ -136,30 +137,31 @@ def update_namaz_time():
                 say_to_serial("Fajr: "+fajr_time_obj.strftime('%I:%M %p'))
             else:
                 print("Failed to find Fajr time.")
-                return None
-    except Exception as e:
+        return None
+    except Exception as e: # pylint: disable=broad-except
         exception_logger("update_namaz_time", e)
 
 
 def read_serial_data(data):
-    global logs_receiving, log_data
+    """read TTGO-Tcall serial data"""
+    global LOGS_RECEIVING, LOG_DATA # pylint: disable=global-statement
     try:
-        global ngrok_link
-        if "{hay orange-pi!" in data or logs_receiving:
-            if "[#SaveIt]:" in data or logs_receiving:
-                logs_receiving = True
+        global CURRENT_NGROK_LINK
+        if "{hay orange-pi!" in data or LOGS_RECEIVING:
+            if "[#SaveIt]:" in data or LOGS_RECEIVING:
+                LOGS_RECEIVING = True
                 print("receiving logs...")
-                log_data += data
+                LOG_DATA += data
                 if "end_of_file" in data:
-                    logs_receiving = False
+                    LOGS_RECEIVING = False
                     print("logs received saving them")
-                    write_in_file("logs.txt", "\nLogs:\n" + log_data +
+                    write_in_file("logs.txt", "\nLogs:\n" + LOG_DATA +
                                   "\nTime stamp: {"+fetch_current_time_online() + "}\n")
-                    log_data = ""
+                    LOG_DATA = ""
             elif "send time" in data or "update time" in data or "send updated time" in data:
                 update_time()
             elif "send ip" in data or "update ip" in data or "my ip" in data:
-                ip = requests.get('https://api.ipify.org').text
+                ip = requests.get('https://api.ipify.org', timeout=10).text
                 msg = '{hay ttgo-tcall! here is the ip: ' + ip + '.}'
                 print(f"sending : {msg}")
                 send_to_serial_port(msg)
@@ -174,28 +176,33 @@ def read_serial_data(data):
                 print(f"Extracted new_message_number: {new_message_number}")
                 if "restart op" in temp_str:
                     print(
-                        f"Asking TTGO to delete the message {new_message_number} and rebooting the system...")
+                        f"Asking TTGO to delete the message "
+                        f"{new_message_number} and rebooting the system...")
                     send_to_serial_port("delete " + new_message_number)
                     time.sleep(3)
                     reboot_system()
                 elif "send ngrok link" in temp_str:
                     print(
-                        f"Asking TTGO to delete the message {new_message_number} and sending ngrok link...")
+                        f"Asking TTGO to delete the message "
+                        f"{new_message_number} and sending ngrok link...")
                     send_to_serial_port("delete " + new_message_number)
-                    print(f"ngrok link: {ngrok_link}")
+                    print(f"ngrok link: {CURRENT_NGROK_LINK}")
                     send_ngrok_link()
             elif "send bypass key" in data:
                 say_to_serial("bypass key: " + os.getenv("BYPASS_KEY"))
             else:
                 print(f"unknown keywords in command: {data}")
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-except
         exception_logger("read_serial_data", e)
 
 
-def fetch_current_time_online():  # TODO: need to separate this part from py_time
+def fetch_current_time_online():
+    # TODO: need to separate this part from py_time # pylint: disable=fixme
+    """Return current time after fetching from online source for TTGO-TCall"""
     try:
         response = requests.get(
-            'http://worldtimeapi.org/api/timezone/Asia/Karachi')
+            'http://worldtimeapi.org/api/timezone/Asia/Karachi', timeout=10)
+
         data = response.json()
         current_time = datetime.datetime.fromisoformat(data['datetime'])
         formatted_time = current_time.strftime("%y/%m/%d,%H:%M:%S")
@@ -206,24 +213,26 @@ def fetch_current_time_online():  # TODO: need to separate this part from py_tim
 
 
 def say_to_serial(serial_data):
+    """Used to communicate with TTGO-TCall via defined string"""
     try:
         serial_data = "{hay ttgo-tcall!"+serial_data+"}"
         print(f"sending : {serial_data}")
         send_to_serial_port(serial_data)
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-except
         exception_logger("say_to_serial", e)
 
 
 def send_to_serial_port(serial_data):
-    # global serial
+    """Will send string to TTGO-TCall"""
     try:
         print(f"Sending data to serial port: {serial_data}")
-        ser.write(serial_data.encode())
-    except serial.SerialException as e:
+        SERIAL_PORT.write(serial_data.encode())
+    except serial.SerialException as e: # pylint: disable=broad-except
         exception_logger("send_to_serial_port", e)
 
 
 def update_time():
+    """Will send updated time to TTGO-TCall"""
     try:
         current_time = fetch_current_time_online()
         if current_time:
@@ -231,52 +240,56 @@ def update_time():
             send_to_serial_port(current_time)
         else:
             print("Failed to fetch current time.")
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-except
         exception_logger("update_time", e)
 
 
 def stop_ngrok():
+    """Need to kill NGROK if it is already running"""
     try:
         print("killing ngrok server...")
-        subprocess.run(['pkill', '-f', 'ngrok'])
-    except Exception as e:
+        subprocess.run(['pkill', '-f', 'ngrok'], check=True)
+    except Exception as e: # pylint: disable=broad-except
         exception_logger("stop_ngrok", e)
 
 
 def send_message(message):
+    """Used to send sms to defined number"""
     try:
         send_to_serial_port("sms " + message)
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-except
         exception_logger("send_message", e)
 
 
 def write_in_file(file_path, content):
+    """Will write data in file"""
     file_path += EXTENSION_TYPE
     content = "\n\n------------------------------\n"+content+"\n" + \
         "{time: " + fetch_current_time_online()+"}\n--------------------------------\n"
     try:
-        with open(file_path, 'a') as file:
+        with open(file_path, 'a', encoding='utf-8') as file:
             file.write(content)
             file.flush()  # Ensure the data is written to the file immediately
-    except PermissionError as pe:
+    except PermissionError:
         # If PermissionError occurs, try to create the file in the current working directory
         current_directory = os.getcwd()
         file_path = os.path.join(current_directory, file_path)
 
-        with open(file_path, 'a') as file:
+        with open(file_path, 'a', encoding='utf-8') as file:
             file.write(content)
             file.flush()  # Ensure the data is written to the file immediately
     except FileNotFoundError:
         # Handle the case where the file doesn't exist
-        with open(file_path, 'w') as file:
+        with open(file_path, 'a', encoding='utf-8') as file:
             file.write(content)
             file.flush()  # Ensure the data is written to the file immediately
-    except Exception as ex:
+    except Exception as ex: # pylint: disable=broad-except
         # Handle other exceptions if needed
         print(f"An error occurred: {ex}")
 
 
 def connected_with_internet():
+    """If module is not connected with internet it will try to connect"""
     while True:
         try:
             # Check internet connectivity by pinging Google's public DNS server
@@ -291,10 +304,13 @@ def connected_with_internet():
 
 
 def exception_logger(function_name, error):
+    """Work as a logger (additional logging with function name)"""
     if connected_with_internet():
         print(
-            f"\n\t\t-----------------------------------------\nHandled Error:\nError occurred: {error} \nin {function_name} function\n\n")
+            f"\n\t\t-----------------------------------------\n"
+            f"Handled Error:\nError occurred: {error}"
+            f" \nin {function_name} function\n\n")
+
         write_in_file(EXCEPTION_LOGGER_FILE,
-                      "Exception occur in{"+function_name+"} function.\n Error message: " + str(error))
-# if __name__ == '__main__':
-#     update_namaz_time()
+                      "Exception occur in{" + function_name + "}"
+                      " function.\n Error message: " + str(error))

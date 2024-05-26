1d37ed083d0a31ea9747aa3f0f75eecb654a0d8b
mhamidjamil <m.hamid.jamil99@gmail.com>
2024-05-26 16:33:03 +0500

    adds ntfy by resolving module not found issue after optimizing code

--- influx_db/influxdb_manager.py
+++ influx_db/influxdb_manager.py
@@ -1,31 +1,33 @@
+import sys
+sys.path.append('/home/orangepi/Desktop/projects/orangepi/serial_communication/web/routes/communication')
+import os
 import requests
 import time
 from datetime import datetime
-import influxdb_client, os, time
+from dotenv import load_dotenv
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-from dotenv import load_dotenv
-# from serial_communication.web.routes.communication.ntfy import send_critical
+from ntfy import send_critical
 import threading
 
-# ThingSpeak API URLs
-temperature_url = "https://api.thingspeak.com/channels/2201589/fields/1.json?results=1"
-humidity_url = "https://api.thingspeak.com/channels/2201589/fields/2.json?results=1"
-
+# Load environment variables
 DOTENV_PATH = '/home/orangepi/Desktop/projects/orangepi/serial_communication/web/.env'
 load_dotenv(DOTENV_PATH)
 
-BUCKET=os.getenv("INFLUXDB_BUCKET")
-ORGANIZATION = os.getenv("INFLUXDB_ORGANIZATION")
+# ThingSpeak API URLs
+TEMPERATURE_URL = "https://api.thingspeak.com/channels/2201589/fields/1.json?results=1"
+HUMIDITY_URL = "https://api.thingspeak.com/channels/2201589/fields/2.json?results=1"
 
-token = os.getenv("INFLUXDB_TOKEN")
-influx_url = os.getenv("LOCALHOST")+":"+os.getenv("INFLUXDB_PORT")
-write_client = influxdb_client.InfluxDBClient(url=influx_url, token=token, org=ORGANIZATION)
-MAX_TEMPERATURE=os.getenv("MAX_TEMPERATURE")
+# InfluxDB configuration
+BUCKET = os.getenv("INFLUXDB_BUCKET")
+ORGANIZATION = os.getenv("INFLUXDB_ORGANIZATION")
+TOKEN = os.getenv("INFLUXDB_TOKEN")
+INFLUX_URL = f"{os.getenv('LOCALHOST')}:{os.getenv('INFLUXDB_PORT')}"
+MAX_TEMPERATURE = float(os.getenv("MAX_TEMPERATURE"))
 
 # Initialize InfluxDB client
-write_client = InfluxDBClient(url=influx_url, token=token, org=ORGANIZATION)
-write_api = write_client.write_api(write_options=SYNCHRONOUS)
+influx_client = InfluxDBClient(url=INFLUX_URL, token=TOKEN, org=ORGANIZATION)
+write_api = influx_client.write_api(write_options=SYNCHRONOUS)
 
 # Store the last entry ID to check for new data
 last_entry_id = None
@@ -34,32 +36,30 @@ def fetch_thingspeak_data(url):
     response = requests.get(url)
     data = response.json()
     feeds = data["feeds"]
-    if feeds:
-        return feeds[0]  # Return the latest entry
-    return None
+    return feeds[0] if feeds else None
 
-def upload_to_influxdb(temp, humidity):
+def upload_to_influxdb(temperature, humidity):
     point = (
         Point("environment")
         .tag("source", "ThingSpeak")
-        .field("temperature", temp)
+        .field("temperature", temperature)
         .field("humidity", humidity)
         .time(datetime.utcnow(), WritePrecision.NS)
     )
     write_api.write(bucket=BUCKET, org=ORGANIZATION, record=point)
-    print(f"Written to InfluxDB: temperature={temp}, humidity={humidity}")
+    print(f"Written to InfluxDB: temperature={temperature}, humidity={humidity}")
 
-def manage_think_speak():
+def manage_thingspeak():
+    global last_entry_id  # pylint: disable=global-statement
     while True:
         try:
-            temp_data = fetch_thingspeak_data(temperature_url)
-            humidity_data = fetch_thingspeak_data(humidity_url)
+            temp_data = fetch_thingspeak_data(TEMPERATURE_URL)
+            humidity_data = fetch_thingspeak_data(HUMIDITY_URL)
 
             if temp_data and humidity_data:
                 temp_entry_id = temp_data["entry_id"]
                 humidity_entry_id = humidity_data["entry_id"]
 
-                global last_entry_id # pylint: disable=global-statement
                 # Check if the latest data is new
                 if last_entry_id is None or temp_entry_id > last_entry_id or humidity_entry_id > last_entry_id:
                     temperature = float(temp_data["field1"])
@@ -71,13 +71,12 @@ def manage_think_speak():
                     # Update the last entry ID
                     last_entry_id = max(temp_entry_id, humidity_entry_id)
                 else:
-                    print("skipping thingspeak as new value is not added")
+                    print("Skipping ThingSpeak as new value is not added")
 
-            # Wait for 3 minutes before checking again
+            # Wait for 2 minutes before checking again
             time.sleep(120)
         except Exception as e: # pylint: disable=broad-except
-            # exception_logger("Unexpected error in manage_think_speak", e)
-            print("Unexpected error in function: manage_think_speak", e)
+            print("Unexpected error in function: manage_thingspeak", e)
 
 def monitor_temperature():
     while True:
@@ -85,21 +84,20 @@ def monitor_temperature():
             temperatures = []
 
             # Loop through all thermal zones and fetch temperature values
-            for zone in range(0, 10):  # Assuming thermal zones are numbered from 0 to 9
+            for zone in range(10):  # Assuming thermal zones are numbered from 0 to 9
                 zone_path = f"/sys/class/thermal/thermal_zone{zone}"
 
                 # Check if the thermal zone exists
                 if os.path.exists(zone_path):
                     with open(os.path.join(zone_path, 'temp'), 'r') as file:
                         # Convert temperature to an integer
-                        temp = int(file.read().strip())
+                        temp = int(file.read().strip()) / 1000
                         temperatures.append(temp)
 
             if temperatures:
-                write_api = write_client.write_api(write_options=SYNCHRONOUS)
                 # Calculate average and maximum temperature values
-                average_temp = sum(temperatures) / len(temperatures) / 1000
-                max_temp = max(temperatures) / 1000
+                average_temp = round(sum(temperatures) / len(temperatures), 2)
+                max_temp = max(temperatures)
 
                 # Create the point to write to InfluxDB
                 point = (
@@ -110,20 +108,23 @@ def monitor_temperature():
                 )
                 write_api.write(bucket=BUCKET, org=ORGANIZATION, record=point)
                 print(f"Written to InfluxDB: avg_temp={average_temp}, max_temp={max_temp}")
-                if int(max_temp) > int(MAX_TEMPERATURE):
-                    # send_critical(f"CPU temperature: {max_temp}")
+                if max_temp > MAX_TEMPERATURE:
+                    send_critical(f"CPU temperature: {max_temp}")
                     print("Over heat alert!")
             else:
                 print("No valid temperature values found.")
 
             time.sleep(3)  # Wait for 3 seconds before the next reading
-        except Exception as e: # pylint: disable=broad-except
-            # exception_logger("monitor_temperature", e)
-            print("unexpected error in function: monitor_temperature")
+        except Exception as e:
+            print("Unexpected error in function: monitor_temperature", e)
 
 if __name__ == "__main__":
-    thread = threading.Thread(target=manage_think_speak)
+    # for stress testing:
+    # sudo apt install stress
+    # stress --cpu 8 --io 4 --vm 4 --vm-bytes 1024M --timeout 30s
+
+    thread1 = threading.Thread(target=manage_thingspeak)
     thread2 = threading.Thread(target=monitor_temperature)
 
-    thread.start()
+    thread1.start()
     thread2.start()
